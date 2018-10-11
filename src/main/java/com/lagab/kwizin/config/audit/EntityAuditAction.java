1e8d98de8d86163ce66fda9f5ea400204fb2ecf0
GABRIEL Ladislas <ladislas.gabriel.ext@humanis.com>
2018-10-11 16:45:31 +0200

    auditing entry

---
+++ src/main/java/com/lagab/kwizin/config/audit/EntityAuditAction.java
@@ -0,0 +1,27 @@
+package com.lagab.kwizin.config.audit;
+
+/**
+ * @author gabriel
+ * @since 11/10/2018.
+ * Enum for the different audit actions
+ */
+public enum EntityAuditAction {
+    CREATE("CREATE"),
+    UPDATE("UPDATE"),
+    DELETE("DELETE");
+
+    private String value;
+
+    EntityAuditAction(final String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value();
+    }
+}
