8820be68ae45447399d3048b2ec4b3581f110819
GABRIEL Ladislas <ladislas.gabriel.ext@humanis.com>
2018-10-12 10:27:35 +0200

    User and authority Entity

--- src/main/java/com/lagab/kwizin/repository/AuthorityRepository.java
+++ src/main/java/com/lagab/kwizin/repository/AuthorityRepository.java
@@ -1,7 +1,7 @@
 package com.lagab.kwizin.repository;
 
 
-import com.lagab.kwizin.domain.User;
+import com.lagab.kwizin.domain.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -9,6 +9,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * @author gabriel
  * @since 11/10/2018.
  */
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
-public interface UserRepository extends JpaRepository<User, Long> {
+@RepositoryRestResource(collectionResourceRel = "authorities", path = "authorities")
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
 }
